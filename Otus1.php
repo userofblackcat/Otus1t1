cb8b95df20efcc519cb1f39fa27fe4d1b362227d
sasha <you@example.com>
2018-01-10 01:11:00 +0300

    third commit

--- Otus1.php
+++ Otus1.php
@@ -9,8 +9,14 @@ class Otus1 {
 	}
 }
 
+// ошибки включены на отображение в этом файле
 ini_set("display_errors", "on");
-$vvod = 4;
+
+
+//var_dump($argv);
+
+//получаем данные из консоли ввода
+$vvod = $argv[1];
 $on = new Otus1();
 $vivod = $on->sqrt1($vvod);
 echo $vivod;
